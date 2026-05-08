4df7a23cbf844875ee97bc84c0d217a11d9aceb1
Billy Doumbouya <192708154+Clinton62395@users.noreply.github.com>
2026-05-08 03:42:02 +0000

    updates first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-
+import sitemap from "vite-plugin-sitemap";
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: "https://site-for-noumanian-school.vercel.app",
+      dynamicRoutes: [
+        "/",
+        "",
+        "about",
+        "/news",
+        "/contact",
+        "levels",
+        "levels/college",
+        "levels/computer",
+        "levels/primary",
+      ],
+    }),
+    tailwindcss(),
+  ],
 });
